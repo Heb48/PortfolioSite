48ea3f7cefdd3d432b800026c5a07e2086483b14
Michael Hebden <michael@bjmdesign.com.au>
2014-01-31 00:56:11 +1100

    progress

--- scripts.js
+++ scripts.js
@@ -117,3 +117,19 @@ $(window).load(function () {
   $(window).resize();
 });
 
+
+
+/* Google Fonts Should use Gesta Typekit instead */
+
+  WebFontConfig = {
+    google: { families: [ 'Ropa+Sans::latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })(); 
